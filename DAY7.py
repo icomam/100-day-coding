396f249b72eb9efe18ea99c2c89183bbec53339e
icomam <53452124+icomam@users.noreply.github.com>
2019-08-26 20:08:37 +0300

    Add files via upload
    درس اليوم السابع

---
+++ DAY7.py
@@ -0,0 +1,30 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello")
+Hello
+>>> print('Hello')
+Hello
+>>> 
+>>> a="Hello"
+>>> print(a)
+Hello
+>>> 
+>>> a=""" Lorem ipsum dolor sit amet,  """
+>>> 
+>>> a=""" Lorem ipsum dolor sit amet , consecteur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+>>> print(a)
+ Lorem ipsum dolor sit amet , consecteur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.
+>>> 
+>>> 
+>>> a="Hello, World!"
+>>> print(a[1])
+e
+>>> print(a[4])
+o
+>>> print(a[2:5])
+llo
+>>> 
