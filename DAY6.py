86616257d56539fbdcdf3a1d574bbd48f98dbe72
icomam <53452124+icomam@users.noreply.github.com>
2019-08-26 19:53:27 +0300

    Add files via upload

---
+++ DAY6.py
@@ -0,0 +1,24 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x=int(1)
+>>> y=int(2.8)
+>>> z=int("3")
+>>> print(x)
+1
+>>> print(y)
+2
+>>> print(z)
+3
+>>> x=float(1)
+>>> y=float(2.1)
+>>> z=float("3")
+>>> w=float("4.2")
+>>> print(x)
+1.0
+>>> print(y)
+2.1
+>>> print(z)
+3.0
+>>> print(w)
+4.2
+>>> 
