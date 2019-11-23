9103b53f8f23db18bbd061b0ebbd057e486ad281
icomam <53452124+icomam@users.noreply.github.com>
2019-11-24 01:20:00 +0300

    Add files via upload

---
+++ DAY27.py
@@ -0,0 +1,153 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #DAY27
+>>> a=33
+>>> b=200
+>>> if b > a :
+	print("b is greater than a")
+
+	
+b is greater than a
+>>> 
+>>> 
+>>> a=33
+>>> b=200
+>>> if b>a:
+    print("b is greater than a")
+
+    
+b is greater than a
+>>> 
+>>> a = 33
+>>> b = 200
+>>> if b > a:
+	print("b is greater than a")
+
+	
+b is greater than a
+>>> a = 33
+>>> b=200
+>>> if b>a:
+	print("b is greater than a")
+	elif a == b:
+		
+SyntaxError: invalid syntax
+>>> 
+>>> a=33
+>>> b=33
+>>> if b>a:
+	print("b is greater than a")
+	elif a == b:
+		
+SyntaxError: invalid syntax
+>>> 
+>>> a = 33
+>>> b = 33
+>>> if b > a:
+	print ("b is greater than a")
+elif a == b:
+	print("a and b are equal")
+
+	
+a and b are equal
+>>> 
+>>> a = 33
+>>> b = 33
+>>> if b>a:
+	print("b is greater than a")
+elif a == b:
+	print("a and b are equal")
+else:
+	print("a is greater than b")
+
+	
+a and b are equal
+>>> 
+>>> 
+>>> 
+>>> a = 33
+>>> b = 33
+>>> if b>a:
+	print("b is greater than a")
+else:
+	print("a is greater than b")
+
+	
+a is greater than b
+>>> 
+>>> 
+>>> a=200
+>>> b=33
+>>> 
+>>> if a>b:print("a is greater than b")
+
+a is greater than b
+>>> 
+>>> a=2
+>>> b=330
+>>> print("A") if a>b else print("B")
+B
+>>> 
+>>> print("A") if a>b else print("=") if a==b else print ("B")
+B
+>>> if a>b and c> a:
+	print ("blth condition are True")
+
+	
+>>> c=500
+>>> if a>b and c> a:
+	print ("blth condition are True")
+
+	
+>>> 
+>>> a=2
+>>> b=330
+>>> c=500
+>>> if a>b and c> a:
+	print ("blth condition are True")
+
+	
+>>> 
+>>> a = 200
+>>> b = 33
+>>> c = 500
+>>> if a>b and c>a:
+	print ("Both condition are True")
+
+	
+Both condition are True
+>>> 
+>>> if a>b or a>c:
+	print ("At least one of the condition is True")
+
+	
+At least one of the condition is True
+>>> 
+>>> 
+>>> a=200
+>>> if a>10:
+	print("A")
+if a>400:
+	
+SyntaxError: invalid syntax
+>>> if a>100:
+	print("A")
+	if a>500:
+		print("B")
+		if a<300:
+			print("True")
+			else:
+				
+SyntaxError: invalid syntax
+>>> a=200
+>>> if a>100:
+	print("True")
+	if a<100:
+		print("True22")
+	else:
+		print("nothing True")
+
+		
+True
+nothing True
+>>> 
